c878ec60e5e76972bfb39d3c20d63edd29927c01
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-10-28 19:51:14 +0200

    Update logger.go

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -6,12 +6,10 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// Logger represents a logger instance
 type Logger struct {
 	logger zerolog.Logger
 }
 
-// NewLogger creates a new logger with the specified log level
 func NewLogger() *Logger {
 	level := zerolog.InfoLevel
 	switch GetLogLevel() {
@@ -37,7 +35,6 @@ func NewLogger() *Logger {
 	return &Logger{logger: logger}
 }
 
-// GetLogLevel returns the log level from environment or default
 func GetLogLevel() string {
 	logLevel := os.Getenv("LOG_LEVEL")
 	if logLevel == "" {
@@ -46,27 +43,22 @@ func GetLogLevel() string {
 	return logLevel
 }
 
-// Debug logs a debug message
 func (l *Logger) Debug() *zerolog.Event {
 	return l.logger.Debug()
 }
 
-// Info logs an info message
 func (l *Logger) Info() *zerolog.Event {
 	return l.logger.Info()
 }
 
-// Warn logs a warning message
 func (l *Logger) Warn() *zerolog.Event {
 	return l.logger.Warn()
 }
 
-// Error logs an error message
 func (l *Logger) Error() *zerolog.Event {
 	return l.logger.Error()
 }
 
-// Fatal logs a fatal message and exits
 func (l *Logger) Fatal() *zerolog.Event {
 	return l.logger.Fatal()
 }
